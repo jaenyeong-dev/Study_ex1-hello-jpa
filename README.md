5b46b34343fb724b3e30a7a710e956575fd48375
noah <noah@space-culture.com>
2020-02-03 15:44:53 +0900

    Persistence context

--- README.md
+++ README.md
@@ -15,10 +15,15 @@
 #### H2 Database
 * 1.4.199
 
+#### error
+* H2 console
+  * 
+* hibernate.properties not found
+
 -----
 
 ### [Contents]
-#### JPA 구동 방식ㅎ
+#### JPA 구동 방식
 * persistence 클래스가 META-INF/persistence.xml 설정 정보 조회 후 EntityManagerFactory 생성
 * EntityManagerFactory가 필요한 entityManager를 생성
 
@@ -39,3 +44,43 @@
 #### JPQL
 * SQL을 추상화한 객체 지향 쿼리 언어 (특정 DBMS SQL에 의존적이지 않음)
 * 엔티티 객체를 대상으로 쿼리 (SQL은 DB 테이블을 대상으로 쿼리)
+
+#### 영속성 컨텍스트
+* 엔티티를 영구 저장하는 환경
+* 논리적인 개념, 엔티티 매니저를 통해 접근 (EntityManager 안에 PersistenceContext가 눈에 보이지 않게 존재)
+* 상태
+  * 비영속(new/transient)
+    * 새로운 상태
+  * 영속(managed)
+    * 영속성 컨텍스트에 관리되는 상태
+  * 준영속(detached)
+    * 영속성 컨텍스트에 저장 되었다가 분리된 상태
+  * 삭제(removed)
+    * 삭제된(DB에서) 상태
+  * 플러시
+    * 영속성 컨텍스트의 변경 내용을 DB에 반영(강제 즉시 반영)
+    * em.flush()
+    * 트랜잭션 커밋, JPQL 쿼리 실행 : 자동 호출
+    * 플러시가 실행되도 1차 캐시는 남아 있음 (영속성 컨텍스트를 비우지 않음)
+    * 플러시 모드 옵션 (거의 사용하지 않음)
+      * em.setFlushMode(AUTO, COMMIT)
+  * 준영속
+    * em.detach(entity) : 해당 엔티티만 1차 캐시에서 제거
+    * em.clear() : 영속성 컨텍스트를 전체 초기화
+    * em.close() : 영속성 컨텍스트 종료
+* 이점
+  * 1차 캐시
+    * DB 하나의 트랜잭션 안에서만 살아있음(2차 캐시(App 전체에서 사용하는)가 아니기 때문에 성능적으로 큰 차이는 없음)
+    * @Id(컬럼 키 값)가 캐시 키 값이 됨
+    * 캐시에 값이 없을 경우 DB에서 가져옴
+  * 동일성 보장
+    * 1차 캐시로 반복 가능한 읽기 등급의 트랜잭션 격리 수준을 DB가 아닌 App 차원에서 제공
+  * 트랜잭션을 지원하는 쓰기 지연
+    * 1차 캐시에 저장한 후 쓰기 지연 SQL 저장소에 먼저 SQL을 작성하여 저장
+    * 트랜잭션을 커밋하는 시점에 쓰기 지연 SQL 저장소에 있는 쿼리 목록이 실행됨
+    * 버퍼링 모아서 한 번에 처리
+  * 변경 감지
+    * 엔티티 수정 시 (Dirty checking) em.update 또는 em.persist 같은 처리 없이 데이터 동기화
+    * 1차 캐시엔 스냅샷이 있음 (최초 영속성 컨텍스트에 들어왔을 당시에 상태를 저장)
+      * 스냅샷과 엔티티 값을 비교 후 처리
+  * 지연 로딩
--- src/main/java/com/jpa/basic/JpaMain.java
+++ src/main/java/com/jpa/basic/JpaMain.java
@@ -4,57 +4,74 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
-	public static void main(String[] args) {
-		// Name은 persistence.xml에 persistence-unit 태그의 이름 속성값을 줌
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaBasic");
+    public static void main(String[] args) {
+        // Name은 persistence.xml에 persistence-unit 태그의 이름 속성값을 줌
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaBasic");
 
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
-		tx.begin();
-		// execute code
-		try {
+        tx.begin();
+        // execute code
+        try {
 
-			// insert
+            // insert (비영속)
 //			Member member = new Member();
 //			member.setId(1L);
 //			member.setName("helloA");
 //			member.setId(2L);
 //			member.setName("helloB");
+
+//			Member member = new Member(101L, "TEST");
 //			em.persist(member);
 
-			// update
-			Member findMember = em.find(Member.class, 1L);
-			System.out.println("findMember id: " + findMember.getId());
-			System.out.println("findMember name: " + findMember.getName());
-			findMember.setName("Hello JPA");
 
-			// delete
+            // 영속
+//			System.out.println(">> Before <<");
+//			em.persist(member);
+//			System.out.println(">> After <<");
+
+            // 준영속
+//			em.detach(member);
+
+            // update
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+            System.out.println("findMember id: " + findMember1.getId());
+            System.out.println("findMember name: " + findMember1.getName());
+
+            // 영속 엔티티 동일성 보장
+            System.out.println("result = " + (findMember1 == findMember2));
+
+            // update
+//			findMember.setName("Hello JPA");
+
+            // delete
 //			em.remove(findMember);
 
-			// JPQL
-			List<Member> result = em.createQuery("select m from Member as m", Member.class)
-					.setFirstResult(5)
-					.setMaxResults(8)
-					.getResultList();
+            // JPQL
+//			List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//					.setFirstResult(5)
+//					.setMaxResults(8)
+//					.getResultList();
+//
+//			for (Member mem : result) {
+//				System.out.println("member.name : " + mem.getName());
+//			}
 
-			for (Member mem : result) {
-				System.out.println("member.name : " + mem.getName());
-			}
+			// 커밋 시점에 insert
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
 
-			tx.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			tx.rollback();
-		} finally {
+            em.close();
+        }
+        emf.close();
 
-			em.close();
-		}
-		emf.close();
-
-	}
+    }
 }
